cba6aaf62b4b7df9207a5b07783b82d15aa7e55c
Vinayak Goel <vinayak21113@iiitd.ac.in>
2023-11-04 12:13:44 +0530

    lines

---
+++ code-7/src/main.cpp
@@ -0,0 +1,383 @@
+// Assignment 03: Lighting and shading
+
+/*References
+  Trackball: http://en.wikibooks.org/wiki/OpenGL_Programming/Modern_OpenGL_Tutorial_Arcball
+*/
+
+#include "utils.h"
+
+#define GLM_FORCE_RADIANS
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/string_cast.hpp>
+#include <iostream>
+using namespace std;
+// Globals
+int screen_width = 640, screen_height = 640;
+GLint vModel_uniform, vView_uniform, vProjection_uniform;
+GLint vColor_uniform;
+glm::mat4 modelT, viewT, projectionT;
+int val; // The model, view and projection transformations
+vector<float> v;
+double oldX, oldY, currentX, currentY;
+bool isDragging = false;
+
+void createCubeObject(unsigned int &, unsigned int &);
+void LoadObj(unsigned int &, unsigned int &);
+void createCage(unsigned int &, unsigned int &);
+
+void setupModelTransformation(unsigned int &);
+void setupViewTransformation(unsigned int &);
+void setupProjectionTransformation(unsigned int &);
+glm::vec3 getTrackBallVector(double x, double y);
+
+int main(int, char **)
+{
+    // Setup window
+    GLFWwindow *window = setupWindow(screen_width, screen_height);
+    ImGuiIO &io = ImGui::GetIO(); // Create IO object
+
+    ImVec4 clearColor = ImVec4(1.0f, 1.0f, 1.0f, 1.00f);
+
+    unsigned int shaderProgram = createProgram("./shaders/vshader.vs", "./shaders/fshader.fs");
+
+    // Get handle to color variable in shader
+    vColor_uniform = glGetUniformLocation(shaderProgram, "vColor");
+    if (vColor_uniform == -1)
+    {
+        fprintf(stderr, "Could not bind location: vColor\n");
+        exit(0);
+    }
+
+    glUseProgram(shaderProgram);
+
+    unsigned int VAO, VAO2;
+    glGenVertexArrays(1, &VAO);
+    glGenVertexArrays(1, &VAO2);
+
+    setupModelTransformation(shaderProgram);
+    setupViewTransformation(shaderProgram);
+    setupProjectionTransformation(shaderProgram);
+
+    //  createCubeObject(shaderProgram, VAO);
+    LoadObj(shaderProgram, VAO2);
+    // createCage(shaderProgram, VAO);
+    //  createCage(shaderProgram, VAO);
+    oldX = oldY = currentX = currentY = 0.0;
+    int prevLeftButtonState = GLFW_RELEASE;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // glBindVertexArray(VAO_Cage);
+        // glBindBuffer(GL_ARRAY_BUFFER, VBO_Cage);
+        // glBufferData(GL_ARRAY_BUFFER, v.size() * sizeof(float), &v[0], GL_DYNAMIC_DRAW);
+        // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+        // glEnableVertexAttribArray(0);
+        glfwPollEvents();
+        createCage(shaderProgram, VAO);
+
+        // Get current mouse position
+        int leftButtonState = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+        double x, y;
+        glfwGetCursorPos(window, &x, &y);
+        if (leftButtonState == GLFW_PRESS && prevLeftButtonState == GLFW_RELEASE)
+        {
+            isDragging = true;
+            currentX = oldX = x;
+            currentY = oldY = y;
+        }
+        else if (leftButtonState == GLFW_PRESS && prevLeftButtonState == GLFW_PRESS)
+        {
+            currentX = x;
+            currentY = y;
+        }
+        else if (leftButtonState == GLFW_RELEASE && prevLeftButtonState == GLFW_PRESS)
+        {
+            isDragging = false;
+        }
+
+        // Rotate based on mouse drag movement
+        prevLeftButtonState = leftButtonState;
+        if (isDragging && (currentX != oldX || currentY != oldY))
+        {
+            glm::vec3 va = getTrackBallVector(oldX, oldY);
+            glm::vec3 vb = getTrackBallVector(currentX, currentY);
+
+            float angle = acos(std::min(1.0f, glm::dot(va, vb)));
+            glm::vec3 axis_in_camera_coord = glm::cross(va, vb);
+            glm::mat3 camera2object = glm::inverse(glm::mat3(viewT * modelT));
+            glm::vec3 axis_in_object_coord = camera2object * axis_in_camera_coord;
+            modelT = glm::rotate(modelT, angle, axis_in_object_coord);
+            glUniformMatrix4fv(vModel_uniform, 1, GL_FALSE, glm::value_ptr(modelT));
+
+            oldX = currentX;
+            oldY = currentY;
+        }
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        glUseProgram(shaderProgram);
+
+        {
+            ImGui::Begin("Information");
+            ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_LINES, 0, sizeof(v) / 3);
+        glBindVertexArray(VAO2);
+
+        glUniform3f(vColor_uniform, 0.5, 0.5, 0.5);
+        glDrawArrays(GL_LINES, 0, val);
+        //
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    cleanup(window);
+
+    return 0;
+}
+void createCage(unsigned int &program, unsigned int &obj_VAO)
+{
+    glUseProgram(program);
+
+    // Bind shader variables
+    int vVertex_attrib = glGetAttribLocation(program, "vVertex");
+    if (vVertex_attrib == -1)
+    {
+        fprintf(stderr, "Could not bind location: vVertex\n");
+        exit(0);
+    }
+    v.push_back(0.0);
+    v.push_back(0.0);
+    v.push_back(0.0);
+
+    v.push_back(8.0);
+    v.push_back(0.0);
+    v.push_back(0.0);
+    v.push_back(8.0);
+    v.push_back(18.0);
+    v.push_back(0.0);
+    v.push_back(-8.0);
+    v.push_back(18.0);
+    v.push_back(0.0);
+    v.push_back(-8.0);
+    v.push_back(0.0);
+    v.push_back(0.0);
+    // v.push_back(8.0);
+    // v.push_back(11.0);
+    // v.push_back(0.0);
+
+    glGenVertexArrays(1, &obj_VAO);
+    glBindVertexArray(obj_VAO);
+    GLuint vertex_VBO;
+    cout << v.size() << endl;
+
+    glGenBuffers(1, &vertex_VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_VBO);
+    glBufferData(GL_ARRAY_BUFFER, v.size() * sizeof(GLfloat), &v[0], GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+void createCubeObject(unsigned int &program, unsigned int &cube_VAO)
+{
+    glUseProgram(program);
+
+    // Bind shader variables
+    int vVertex_attrib = glGetAttribLocation(program, "vVertex");
+    if (vVertex_attrib == -1)
+    {
+        fprintf(stderr, "Could not bind location: vVertex\n");
+        exit(0);
+    }
+
+    // Cube data
+    GLfloat cube_vertices[] = {10, 10, -10, -10, 10, -10, -10, -10, -10, 10, -10, -10, // Front
+                               10, 10, 10, -10, 10, 10, -10, -10, 10, 10, -10, 10};    // Back
+    GLushort cube_indices[] = {
+        0, 1, 2, 0, 2, 3, // Front
+        4, 7, 5, 5, 7, 6, // Back
+        1, 6, 2, 1, 5, 6, // Left
+        0, 3, 4, 4, 7, 3, // Right
+        0, 4, 1, 4, 5, 1, // Top
+        2, 6, 3, 3, 6, 7  // Bottom
+    };
+
+    // Generate VAO object
+    glGenVertexArrays(1, &cube_VAO);
+    glBindVertexArray(cube_VAO);
+
+    // Create VBOs for the VAO
+    // Position information (data + format)
+    int nVertices = (6 * 2) * 3; //(6 faces) * (2 triangles each) * (3 vertices each)
+    GLfloat *expanded_vertices = new GLfloat[nVertices * 3];
+    for (int i = 0; i < nVertices; i++)
+    {
+        expanded_vertices[i * 3] = cube_vertices[cube_indices[i] * 3];
+        expanded_vertices[i * 3 + 1] = cube_vertices[cube_indices[i] * 3 + 1];
+        expanded_vertices[i * 3 + 2] = cube_vertices[cube_indices[i] * 3 + 2];
+    }
+    GLuint vertex_VBO;
+    glGenBuffers(1, &vertex_VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_VBO);
+    glBufferData(GL_ARRAY_BUFFER, nVertices * 3 * sizeof(GLfloat), expanded_vertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(vVertex_attrib);
+    glVertexAttribPointer(vVertex_attrib, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    delete[] expanded_vertices;
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0); // Unbind the VAO to disable changes outside this function.
+}
+void LoadObj(unsigned int &program, unsigned int &obj_VAO)
+{
+    FILE *ObjFile = fopen("BEAR2.obj", "r");
+    if (ObjFile == NULL)
+    {
+        cout << "Error Opening Obj File";
+        return;
+    }
+    glUseProgram(program);
+
+    // Bind shader variables
+    int vVertex_attrib = glGetAttribLocation(program, "vVertex");
+    if (vVertex_attrib == -1)
+    {
+        fprintf(stderr, "Could not bind location: vVertex\n");
+        exit(0);
+    }
+
+    vector<float> VertexDataTemp;
+    vector<float> FaceDataTemp;
+    while (true)
+    {
+        char data[1024];
+        int recv = fscanf(ObjFile, "%s\n", data);
+        if (recv < 0)
+        {
+            break;
+        }
+
+        glm::vec3 vertices;
+        glm::vec4 faces;
+        if (!strcmp(data, "v"))
+        {
+            fscanf(ObjFile, "%f %f %f\n", &vertices.x, &vertices.y, &vertices.z);
+            VertexDataTemp.push_back(vertices.x * 2);
+            VertexDataTemp.push_back(vertices.y * 2);
+            VertexDataTemp.push_back(vertices.z * 2);
+        }
+        if (!strcmp(data, "f"))
+        {
+            float f1, f2, f3, f4, f5, f6;
+            fscanf(ObjFile, "%f/%f %f/%f %f/%f \n", &f1, &f2, &f3, &f4, &f5, &f6);
+
+            FaceDataTemp.push_back(f1 - 1);
+            FaceDataTemp.push_back(f3 - 1);
+            FaceDataTemp.push_back(f5 - 1);
+        }
+    }
+    glGenVertexArrays(1, &obj_VAO);
+    glBindVertexArray(obj_VAO);
+    float v[FaceDataTemp.size() * 3];
+    val = FaceDataTemp.size();
+    for (int i = 0; i < FaceDataTemp.size(); i++)
+    {
+        v[i * 3] = VertexDataTemp[FaceDataTemp[i] * 3];
+        v[i * 3 + 1] = VertexDataTemp[FaceDataTemp[i] * 3 + 1];
+        v[i * 3 + 2] = VertexDataTemp[FaceDataTemp[i] * 3 + 2];
+    }
+    cout << VertexDataTemp.size() << " " << FaceDataTemp.size() << endl;
+
+    GLuint vertex_VBO;
+    glGenBuffers(1, &vertex_VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_VBO);
+    glBufferData(GL_ARRAY_BUFFER, FaceDataTemp.size() * 3 * sizeof(float), v, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0); // Unbind the VAO to disable changes outside this function.
+}
+
+void setupModelTransformation(unsigned int &program)
+{
+    // Modelling transformations (Model -> World coordinates)
+    modelT = glm::translate(glm::mat4(1.0f), glm::vec3(0.0, 0.0, 0.0)); // Model coordinates are the world coordinates
+
+    // Pass on the modelling matrix to the vertex shader
+    glUseProgram(program);
+    vModel_uniform = glGetUniformLocation(program, "vModel");
+    if (vModel_uniform == -1)
+    {
+        fprintf(stderr, "Could not bind location: vModel\n");
+        exit(0);
+    }
+    glUniformMatrix4fv(vModel_uniform, 1, GL_FALSE, glm::value_ptr(modelT));
+}
+
+void setupViewTransformation(unsigned int &program)
+{
+    // Viewing transformations (World -> Camera coordinates
+    // Camera at (0, 0, 100) looking down the negative Z-axis in a right handed coordinate system
+    viewT = glm::lookAt(glm::vec3(40.0, -40.0, 40.0), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+
+    // Pass-on the viewing matrix to the vertex shader
+    glUseProgram(program);
+    vView_uniform = glGetUniformLocation(program, "vView");
+    if (vView_uniform == -1)
+    {
+        fprintf(stderr, "Could not bind location: vView\n");
+        exit(0);
+    }
+    glUniformMatrix4fv(vView_uniform, 1, GL_FALSE, glm::value_ptr(viewT));
+}
+
+void setupProjectionTransformation(unsigned int &program)
+{
+    // Projection transformation
+    projectionT = glm::perspective(45.0f, (GLfloat)screen_width / (GLfloat)screen_height, 0.1f, 1000.0f);
+
+    // Pass on the projection matrix to the vertex shader
+    glUseProgram(program);
+    vProjection_uniform = glGetUniformLocation(program, "vProjection");
+    if (vProjection_uniform == -1)
+    {
+        fprintf(stderr, "Could not bind location: vProjection\n");
+        exit(0);
+    }
+    glUniformMatrix4fv(vProjection_uniform, 1, GL_FALSE, glm::value_ptr(projectionT));
+}
+
+glm::vec3 getTrackBallVector(double x, double y)
+{
+    glm::vec3 p = glm::vec3(2.0 * x / screen_width - 1.0, 2.0 * y / screen_height - 1.0, 0.0); // Normalize to [-1, +1]
+    p.y = -p.y;                                                                                // Invert Y since screen coordinate and OpenGL coordinates have different Y directions.
+
+    float mag2 = p.x * p.x + p.y * p.y;
+    if (mag2 <= 1.0f)
+        p.z = sqrtf(1.0f - mag2);
+    else
+        p = glm::normalize(p); // Nearest point, close to the sides of the trackball
+    return p;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -13,239 +13,244 @@
 #include <iostream>
 using namespace std;
 
-GLfloat MeshVertices[200000 * 3];
-GLfloat cageVertices[200000*3];
+GLfloat MeshVertices[1401 * 3];
+GLfloat cageVertices[1401 * 3];
 
-GLuint meshFace[200000*3];
-GLuint cageFace[200000*3];
-
+GLuint meshFace[2723 * 3];
+GLuint cageFace[2723 * 3];
+vector<float> v;
 int numVertices;
 int numFaces;
 
 Mesh *testCubeMesh()
 {
-	GLfloat verts[] = {0, 0, 0,
-					   1, 0, 0,
-					   1, 1, 0,
-					   0, 1, 0,
-					   0, 0, 1,
-					   1, 0, 1,
-					   1, 1, 1,
-					   0, 1, 1};
+    GLfloat verts[] = {0, 0, 0,
+                       1, 0, 0,
+                       1, 1, 0,
+                       0, 1, 0,
+                       0, 0, 1,
+                       1, 0, 1,
+                       1, 1, 1,
+                       0, 1, 1};
 
-	GLuint indices[] = {0, 1, 2,
-						0, 2, 3,
-						4, 5, 6,
-						4, 6, 7,
-						0, 4, 7,
-						0, 7, 3,
-						1, 5, 6,
-						1, 6, 2,
-						0, 1, 5,
-						0, 5, 4,
-						3, 2, 6,
-						3, 6, 7};
+    GLuint indices[] = {0, 1, 2,
+                        0, 2, 3,
+                        4, 5, 6,
+                        4, 6, 7,
+                        0, 4, 7,
+                        0, 7, 3,
+                        1, 5, 6,
+                        1, 6, 2,
+                        0, 1, 5,
+                        0, 5, 4,
+                        3, 2, 6,
+                        3, 6, 7};
 
-	return new Mesh(verts, indices, 8, 12);
+    return new Mesh(verts, indices, 8, 12);
 }
 Mesh *LoadObj()
 {
-	FILE *ObjFile = fopen("polygon.obj", "r");
-	if (ObjFile == NULL)
-	{
-		cout << "Error Opening Obj File";
-	}
+    FILE *ObjFile = fopen("BEAR2.obj", "r");
+    if (ObjFile == NULL)
+    {
+        cout << "Error Opening Obj File";
+    }
 
-	GLfloat VertexDataTemp[200000 * 3];
-	GLuint FaceDataTemp[200000 * 3];
-	int i = 0, j = 0;
-	while (true)
-	{
-		char data[1024];
-		int recv = fscanf(ObjFile, "%s\n", data);
-		//	cout << data << endl;
-		if (recv < 0)
-		{
-			break;
-		}
+    GLfloat VertexDataTemp[1401 * 3];
+    GLuint FaceDataTemp[2723 * 3];
+    int i = 0, j = 0;
+    while (true)
+    {
+        char data[1024];
+        int recv = fscanf(ObjFile, "%s\n", data);
+        //	cout << data << endl;
+        if (recv < 0)
+        {
+            break;
+        }
 
-		glm::vec3 vertices;
+        glm::vec3 vertices;
 
-		if (!strcmp(data, "v"))
-		{
-			fscanf(ObjFile, "%f %f %f\n", &vertices.x, &vertices.y, &vertices.z);
-			VertexDataTemp[i++] = (vertices.x)*0.042;
-			VertexDataTemp[i++] = (vertices.y)*0.042;
-			VertexDataTemp[i++] = (vertices.z)*0.042;
-		}
+        if (!strcmp(data, "v"))
+        {
+            fscanf(ObjFile, "%f %f %f\n", &vertices.x, &vertices.y, &vertices.z);
+            VertexDataTemp[i++] = (vertices.x) * 0.1;
+            VertexDataTemp[i++] = (vertices.y) * 0.1;
+            VertexDataTemp[i++] = (vertices.z) * 0.1;
+        }
 
-		
+        if (!strcmp(data, "f"))
+        {
+            float f1, f2, f3, f4, f5, f6;
+            fscanf(ObjFile, "%f/%f %f/%f %f/%f\n", &f1, &f4, &f2, &f5, &f3, &f6);
 
-		if (!strcmp(data, "f"))
-		{
-			float f1, f2, f3, f4, f5, f6;
-			fscanf(ObjFile, "%f %f %f\n", &f1, &f2, &f3);
+            FaceDataTemp[j++] = (f1 - 1);
+            FaceDataTemp[j++] = (f2 - 1);
+            FaceDataTemp[j++] = (f3 - 1);
+        }
+    }
 
-			FaceDataTemp[j++] = (f1 - 1);
-			FaceDataTemp[j++] = (f2 - 1);
-			FaceDataTemp[j++] = (f3 - 1);
-		}
-		
-	}
+    for (int i = 0; i < 1401 * 3; i++)
+    {
+        MeshVertices[i] = VertexDataTemp[i];
+    }
 
-	for(int i = 0;i<200000*3;i++){
-			MeshVertices[i] = VertexDataTemp[i];
-		}
+    for (int i = 0; i < 2723 * 3; i++)
+    {
+        meshFace[i] = FaceDataTemp[i];
+    }
 
-	for(int i = 0;i<200000*3;i++){
-		meshFace[i] = FaceDataTemp[i];
-	}
+    numVertices = i;
+    numFaces = j;
 
+    cout << i << " " << j << endl;
+    // float v[j * 3];
 
-numVertices = i;
-numFaces = j;
-	
-	cout << i << " " << j << endl;
-	// float v[j * 3];
-
-	// for (int k = 0; k < j; k++)
-	// {
-	// 	v[k * 3] = VertexDataTemp[FaceDataTemp[k] * 3];
-	// 	v[k * 3 + 1] = VertexDataTemp[FaceDataTemp[k] * 3 + 1];
-	// 	v[k * 3 + 2] = VertexDataTemp[FaceDataTemp[k] * 3 + 2];
-	// }
-	return new Mesh(VertexDataTemp, FaceDataTemp, i / 3, j / 3);
+    // for (int k = 0; k < j; k++)
+    // {
+    // 	v[k * 3] = VertexDataTemp[FaceDataTemp[k] * 3];
+    // 	v[k * 3 + 1] = VertexDataTemp[FaceDataTemp[k] * 3 + 1];
+    // 	v[k * 3 + 2] = VertexDataTemp[FaceDataTemp[k] * 3 + 2];
+    // }
+    return new Mesh(VertexDataTemp, FaceDataTemp, i / 3, j / 3);
 }
 
-Mesh* createCage(){
+void createCage()
+{
+    v.push_back(1.0f);
+    v.push_back(1.0f);
+    v.push_back(0.0f);
+    v.push_back(2.0f);
+    v.push_back(1.0f);
+    v.push_back(0.0f);
+    v.push_back(-1.0f);
+    v.push_back(1.0f);
+    v.push_back(0.0f);
 
-	for(int i = 0;i<200000*3;i++){
-		cageVertices[i] = MeshVertices[i];
-	}
+    // for (int i = 0; i < 200000 * 3; i += 3)
+    // {
+    //     float cageX = cageVertices[i];
+    //     float cageY = cageVertices[i + 1];
+    //     float cageZ = cageVertices[i + 2];
 
-	for(int i = 0;i<200000*3;i+=3){
-		float cageX = cageVertices[i];
-		float cageY = cageVertices[i+1];
-		float cageZ = cageVertices[i+2];
+    //     if (cageX >= 0 && cageY >= 0)
+    //     {
+    //         cageX += 0.3;
+    //         cageY += 0.3;
+    //         cageZ -= 0.8;
+    //     }
 
-		if(cageX>=0 && cageY>=0){
-			cageX+=0.3;
-			cageY+=0.3;
-			cageZ-=0.8;
-		}
+    //     else if (cageX >= 0 && cageY <= 0)
+    //     {
+    //         cageX += 0.3;
+    //         cageY -= 0.3;
+    //         cageZ -= 10;
+    //     }
 
-		else if(cageX>=0 && cageY<=0){
-			cageX+=0.3;
-			cageY-=0.3;
-			cageZ-=10;
-		}
+    //     else if (cageX <= 0 && cageY >= 0)
+    //     {
+    //         cageX -= 0.3;
+    //         cageY += 0.3;
+    //         cageZ -= 10;
+    //     }
 
-		else if(cageX<=0 && cageY>=0){
-			cageX-=0.3;
-			cageY+=0.3;
-			cageZ-=10;
-		}
+    //     else if (cageX <= 0 && cageY <= 0)
+    //     {
+    //         cageX -= 0.3;
+    //         cageY -= 0.3;
+    //         cageZ -= 0.8;
+    //     }
 
-		else if(cageX<=0 && cageY<=0){
-			cageX-=0.3;
-			cageY-=0.3;
-			cageZ-=0.8;
-		}
-		
-		// cageVertices[i] = cageX;
-		// cageVertices[i+1] = cageY;
-		// cageVertices[i+2] = cageZ;
-		
-	}
+    //     // cageVertices[i] = cageX;
+    //     // cageVertices[i+1] = cageY;
+    //     // cageVertices[i+2] = cageZ;
+    // }
 
-	return new Mesh(cageVertices,meshFace, 200000, 200000);
+    // return new Mesh(cageVertices, meshFace, 200000, 200000);
 }
 
 int main(int, char **)
 {
-	// Setup window
-	GLFWwindow *window = setupWindow(SCREEN_W, SCREEN_H, "Parametric Representations of Surfaces");
-	ImGuiIO &io = ImGui::GetIO(); // Create IO
+    // Setup window
+    GLFWwindow *window = setupWindow(SCREEN_W, SCREEN_H, "Parametric Representations of Surfaces");
+    ImGuiIO &io = ImGui::GetIO(); // Create IO
 
-	double last_time = 0;
-	double delta_time = 0;
+    double last_time = 0;
+    double delta_time = 0;
 
-	unsigned int shader_program = createProgram("shaders/vshader.vs", "shaders/fshader.fs");
+    unsigned int shader_program = createProgram("shaders/vshader.vs", "shaders/fshader.fs");
 
-	
+    Camera *cam = new Camera(glm::vec3(0.0f, 3.0f, 3.5f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f),
+                             45.0f, 0.1f, 10000.0f, window);
 
-	Camera *cam = new Camera(glm::vec3(0.0f, 3.0f, 3.5f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f),
-							 45.0f, 0.1f, 10000.0f, window);
+    cam->setProjectionTransformation(shader_program);
+    cam->setViewTransformation(shader_program);
 
-	cam->setProjectionTransformation(shader_program);
-	cam->setViewTransformation(shader_program);
+    // LoadObj();
+    Mesh *mesh = LoadObj();
+    createCage();
+    // Mesh *cage = createCage();
+    // Render a sphere using parametric representation.
+    //  ParametricMesh *mesh = new ParametricMesh(100);
+    // createCage();
 
-	// LoadObj();
-	Mesh *mesh = LoadObj();
-	Mesh *cage = createCage();
-	// Render a sphere using parametric representation.
-	//  ParametricMesh *mesh = new ParametricMesh(100);
-	// createCage();
+    glUseProgram(shader_program);
 
-
-	glUseProgram(shader_program);
-
-	 unsigned int VBO_controlPoints, VBO_Cage;
+    unsigned int VBO_controlPoints, VBO_Cage;
     unsigned int VAO_controlPoints, VAO_Cage;
     glGenBuffers(1, &VBO_controlPoints);
     glGenVertexArrays(1, &VAO_controlPoints);
-    
+
     glGenBuffers(1, &VBO_Cage);
     glGenVertexArrays(1, &VAO_Cage);
 
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    while (!glfwWindowShouldClose(window))
+    {
+        glBindVertexArray(VAO_Cage);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO_Cage);
+        glBufferData(GL_ARRAY_BUFFER, v.size() * sizeof(float), &v[0], GL_DYNAMIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+        glEnableVertexAttribArray(0); // Enable first attribute buffer (vertices)
 
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_BLEND);
-	while (!glfwWindowShouldClose(window))
-	{
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
 
-		glBindVertexArray(VAO_Cage);
-		glBindBuffer(GL_ARRAY_BUFFER, VBO_Cage);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(cageVertices), &cageVertices[0], GL_DYNAMIC_DRAW);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-		glEnableVertexAttribArray(0); //Enable first attribute buffer (vertices)
-		// Start the Dear ImGui frame
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
+        double curr_time = static_cast<double>(glfwGetTime());
+        delta_time = curr_time - last_time;
+        last_time = curr_time;
 
-		double curr_time = static_cast<double>(glfwGetTime());
-		delta_time = curr_time - last_time;
-		last_time = curr_time;
+        if (!io.WantCaptureMouse)
+        {
+            cam->process_input(window, delta_time);
+            cam->setViewTransformation(shader_program);
+        }
 
-		if (!io.WantCaptureMouse)
-		{
-			cam->process_input(window, delta_time);
-			cam->setViewTransformation(shader_program);
-		}
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(WHITE.x, WHITE.y, WHITE.z, WHITE.w);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// Rendering
-		ImGui::Render();
-		int display_w, display_h;
-		glfwGetFramebufferSize(window, &display_w, &display_h);
-		glViewport(0, 0, display_w, display_h);
-		glClearColor(WHITE.x, WHITE.y, WHITE.z, WHITE.w);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // mesh->draw(shader_program);
 
-		mesh->draw(shader_program);
+        //   cage->draw(shader_program);
 
-		cage->draw(shader_program);
+        glBindVertexArray(VAO_Cage);
+        glDrawArrays(GL_LINE_STRIP, 0, sizeof(v) / 3);
 
-		glBindVertexArray(VAO_Cage);
-    glDrawArrays(GL_LINES, 0, sizeof(cageVertices)/3); 
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
+    // Cleanup
+    cleanup(window);
 
-	// Cleanup
-	cleanup(window);
-
-	return 0;
+    return 0;
 }
